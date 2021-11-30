1b77fc38f9bd25975f18e4ebdd6603d65028a582
Maxim Potekhin <potekhin@bnl.gov>
2021-11-29 20:14:53 -0500

    Added a plot

--- check_csv.py
+++ check_csv.py
@@ -22,3 +22,16 @@ if input=='':
     print('Please specify a filename')
     exit(-1)
 
+# Load the dataset
+dataset = loadtxt(input, delimiter=',')
+x = np.linspace(0, 30, 31)
+
+plt.style.use('seaborn-whitegrid')
+
+plt.plot(x, dataset[0], 'o', color='black')
+plt.plot(x, dataset[1], 'o', color='red')
+plt.plot(x, dataset[2], 'o', color='blue')
+plt.plot(x, dataset[3], 'o', color='magenta')
+plt.show()
+
+exit(0)
